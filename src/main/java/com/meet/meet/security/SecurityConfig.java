80a4c0bb7cc44d5c090d0ab4cfbe182622638fb6
sabuhibrahim <sabuhibrahim654@gmail.com>
2023-09-30 12:24:56 +0400

    Enable csrf

--- src/main/java/com/meet/meet/security/SecurityConfig.java
+++ src/main/java/com/meet/meet/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
             .authorizeHttpRequests(
                 authorize -> authorize
                             .requestMatchers("/login", "/register", "/groups", "/css/**", "/js/**", "/uploads/**")
